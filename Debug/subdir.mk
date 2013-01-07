1019e01d2e35c53549850c83ff62a6cda97642ee
oscar <oscar@lubuntu-K70IC>
2013-01-07 16:08:42 +0100

    Debug añadido!

---
+++ Debug/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../luces.c \
+../matematicas.c \
+../material.c \
+../modelado.c \
+../otras_escenas.c \
+../revolucion.c \
+../texturas.c 
+
+OBJS += \
+./luces.o \
+./matematicas.o \
+./material.o \
+./modelado.o \
+./otras_escenas.o \
+./revolucion.o \
+./texturas.o 
+
+C_DEPS += \
+./luces.d \
+./matematicas.d \
+./material.d \
+./modelado.d \
+./otras_escenas.d \
+./revolucion.d \
+./texturas.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
